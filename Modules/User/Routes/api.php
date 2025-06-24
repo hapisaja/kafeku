8fd040b5da1ec1ca5023e40a74c1f4152f5c38dd
hapisaja <piahafis@gmail.com>
2025-06-25 01:06:15 +0700

    Menambahkan fitur CRUD User dan aksesnya

--- Modules/User/Routes/api.php
+++ Modules/User/Routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Modules\User\Controllers\UserController;
 
-Route::middleware(['auth:sanctum'])->group(function () {
+Route::middleware(['auth:sanctum', 'role:admin'])->group(function () {
     Route::get('/users', [UserController::class, 'index']);       // List user
     Route::post('/users', [UserController::class, 'store']);       // Tambah user
     Route::put('/users/{id}', [UserController::class, 'update']);  // Update user
